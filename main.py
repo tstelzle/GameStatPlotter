804161a1d9b825e13eecfb43040a4ad04dfc877f
Tarek Stelzle <tarek.stelzle@gmx.de>
2020-12-06 00:44:31 +0100

    Added plot for all data

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import glob
 import matplotlib.pyplot as plt
 import os
 import sys
+from datetime import datetime
 
 import format_data as formatter
-
-list_day = []
 
 
 def read_all_data():
     path = 'data/formatted'
     all_files = glob.glob(path + '/*.csv')
+    all_data = []
 
     for filename in all_files:
         df = pd.read_csv(filename, index_col=None, header=0)
-        list_day.append(df)
+        my_dict = {filename.split('/')[2].split('.')[0]: df}
+        all_data.append(my_dict)
+
+    return all_data
 
 
 def read_single_data(filename):
@@ -53,6 +56,30 @@ def get_max(chosen_dataframe: pd.DataFrame):
     return maximum
 
 
+def create_plot_for_all_days():
+    data = read_all_data()
+    days = []
+
+    plt.xlabel('Spieltag')
+    plt.ylabel('Punkte')
+    plt.title('Skatspiele')
+
+    for elem in data:
+        for key, value in elem.items():
+            days.append(key)
+
+            for player in get_player_columns(value):
+                player_name = player.split(' - ')[1]
+                player_result = value[player].iloc[-1]
+                plt.scatter(key, player_result, label=player_name)
+
+    days.sort(key=lambda date: datetime.strptime(date, '%d-%m-%Y'))
+    plt.xticks(days)
+    plt.legend()
+    plt.savefig('data/diagramms/' + 'all_data', dpi=300)
+    plt.show()
+
+
 def create_plot_for_day(day: str):
     formatter.format_file(day)
     file = 'data/formatted/' + day + '.csv'
@@ -81,14 +108,16 @@ def create_diagramm_folder():
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('Specify .csv file to read data from.')
-        return
-    else:
-        filename = sys.argv[1]
-
     create_diagramm_folder()
-    create_plot_for_day(filename)
+
+    if len(sys.argv) == 1:
+        create_plot_for_all_days()
+    elif len(sys.argv) == 2:
+        filename = sys.argv[1]
+        create_plot_for_day(filename)
+    else:
+        print('Too many options.')
+        return
 
 
 if __name__ == '__main__':
